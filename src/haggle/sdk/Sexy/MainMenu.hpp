90ee70103d2bbd3ab2ef5a474e22c71b10bb8cb4
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2022-07-19 17:01:46 -0500

    MainMenu

---
+++ src/haggle/sdk/Sexy/MainMenu.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace Sexy
+{
+	struct MainMenu_
+	{
+		char data[244];
+	};
+
+	class __declspec(dllexport) MainMenu
+	{
+	public:
+		static MainMenu* main_menu;
+		static void setup();
+		static bool check_exists();
+
+		static void StartGame();
+	};
+};
