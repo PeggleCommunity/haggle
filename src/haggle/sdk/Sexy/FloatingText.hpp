b7e5659d64011c91c136f457744773b6f195ab4d
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2023-04-08 13:48:19 -0500

    build(haggle): renamed FloatingText struct membs

--- src/haggle/sdk/Sexy/FloatingText.hpp
+++ src/haggle/sdk/Sexy/FloatingText.hpp
@@ -8,14 +8,14 @@ namespace Sexy
 		char padding_0[96];
 		uint32_t color;
 		int unk_0;
-		int unk_1;
+		int time;
 		int unk_2;
 		int unk_3;
 		int unk_4;
 		int unk_5;
 		char padding_2[12];
 		int unk_9;
-		float float_offset_start;
+		float float_speed;
 		char padding_3[156];
 	};
 
