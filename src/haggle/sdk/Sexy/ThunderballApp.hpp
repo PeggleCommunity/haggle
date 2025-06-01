85f56fa2bcde674f7b1b70ee39d7cc9dd75a0b26
Dylan <35438152+amooose@users.noreply.github.com>
2025-05-31 22:33:34 -0400

    fixed Thunderball address being 0 on startup and added PlaySound function

--- src/haggle/sdk/Sexy/ThunderballApp.hpp
+++ src/haggle/sdk/Sexy/ThunderballApp.hpp
@@ -14,6 +14,8 @@ namespace Sexy
 		static void DoToMenu();
 		static int ShowLevelScreen(bool a2);
 		static int ShowBoard(bool a2, bool a3);
+		static void PlaySong(int a2, char a3);
+		static void PlaySong(Sexy::ThunderballApp* thunderball, int a2, char a3);
 		static int ShowBoard(Sexy::ThunderballApp* thudnerball, bool a2, bool a3);
 		static void SetColorblind(bool what);
 	};
